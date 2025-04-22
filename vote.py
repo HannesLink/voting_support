5b2cc75679a60144cd6139158600eba0fd730022
Johannes Link <hannes1977@googlemail.com>
2025-04-22 21:07:43 +0200

    Works on Raspberry Pi

--- vote.py
+++ vote.py
@@ -5,11 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 import pprint
 
-# # Create virtual display
-# from pyvirtualdisplay import Display
-# display = Display(visible=0, size=(800, 600))
-# display.start()
-
 # For further enhancements
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -20,10 +15,9 @@ options = webdriver.ChromeOptions()
 options.add_argument("--headless=new")
 driver = webdriver.Chrome(service=service, options=options)
 
-WebDriverWait(driver, 10).until(
-    EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".u-button"))
-)
-
+# Workaround for item not clickable
+# https://www.testim.io/blog/selenium-element-is-not-clickable-at-point/
+driver.maximize_window()
 
 # navigate to the target webpage
 driver.get("https://lms.handwerkswettbewerb.de/ibt/myso/cty/area=site/de/bin/public/voting/details.ibtsico?path=ibt:/division/myso/op/master/voting/2024/1246419951")
@@ -31,13 +25,10 @@ driver.get("https://lms.handwerkswettbewerb.de/ibt/myso/cty/area=site/de/bin/pub
 #print(driver.page_source)
 
 # Navigate to second page and vote
-voteButton = driver.find_element("xpath", '//button[@title="Stimme abgeben"]')
-print(dir(voteButton))
-print("JL->",voteButton.get_attribute('innerHTML'))
-print(voteButton.get_attribute)
-voteButton.click()
-voteButton2 = driver.find_element("xpath", '//button[@title="Stimme abgeben"]')
-voteButton2.click()
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@title='Stimme abgeben']"))).click()
+driver.implicitly_wait(2)
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@title='Stimme abgeben']"))).click()
+driver.implicitly_wait(2)
 
 # Try to get success message
 resultMessage = driver.find_element(By.CLASS_NAME, "u-value")
@@ -47,4 +38,3 @@ print(resultMessage.get_attribute('innerHTML'))
 
 # close the browser
 driver.quit()
-
