e4b131f7ba1c9298d1e4ea41e8681868605aa09b
Johannes Link <hannes1977@googlemail.com>
2025-04-22 19:40:14 +0200

    Work in progress

--- vote.py
+++ vote.py
@@ -3,25 +3,41 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+import pprint
+
+# # Create virtual display
+# from pyvirtualdisplay import Display
+# display = Display(visible=0, size=(800, 600))
+# display.start()
 
 # For further enhancements
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 # set up chrome driver
-service = Service()
+service = Service(executable_path='/usr/lib/chromium-browser/chromedriver')
 options = webdriver.ChromeOptions()
 options.add_argument("--headless=new")
 driver = webdriver.Chrome(service=service, options=options)
 
+WebDriverWait(driver, 10).until(
+    EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".u-button"))
+)
+
+
 # navigate to the target webpage
 driver.get("https://lms.handwerkswettbewerb.de/ibt/myso/cty/area=site/de/bin/public/voting/details.ibtsico?path=ibt:/division/myso/op/master/voting/2024/1246419951")
 
+#print(driver.page_source)
+
 # Navigate to second page and vote
-voteButton = driver.find_element("xpath", '//*[@title="Stimme abgeben"]')
-voteButton.click();
-voteButton2 = driver.find_element("xpath", '//*[@title="Stimme abgeben"]')
-voteButton2.click();
+voteButton = driver.find_element("xpath", '//button[@title="Stimme abgeben"]')
+print(dir(voteButton))
+print("JL->",voteButton.get_attribute('innerHTML'))
+print(voteButton.get_attribute)
+voteButton.click()
+voteButton2 = driver.find_element("xpath", '//button[@title="Stimme abgeben"]')
+voteButton2.click()
 
 # Try to get success message
 resultMessage = driver.find_element(By.CLASS_NAME, "u-value")
