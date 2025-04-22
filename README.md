6647dd1a576dd61cd402e76f74169bad3fa204c1
Johannes Link <hannes1977@googlemail.com>
2025-04-22 21:10:34 +0200

    Updates for Raspberry Pi (#1)
    * Work in progress
    
    * Works on Raspberry Pi

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # voting_support
 Documentation not yet available
+
+## Prepare Raspberry Pi
+1. Remove current Chromium Version
+   ```sudo apt-get remove chromium chromium-browser rpi-chromium-mods```
+2. Install fixed version
+   ```sudo apt-get install chromium=134.0.6998.35-1~deb12u1 chromium-common=134.0.6998.35-1~deb12u1```
+3. Download proper chromedriver
+   ```curl -LO https://github.com/electron/electron/releases/download/v35.2.0/chromedriver-v35.2.0-linux-arm64.zip```
+4. Extract and install chromedriver
+   ```unzip -x chromedriver-v35.2.0-linux-arm64.zip && sudo mv chromedriver /usr/lib/chromium-browser/```
--- vote.py
+++ vote.py
@@ -3,25 +3,32 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+import pprint
 
 # For further enhancements
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 # set up chrome driver
-service = Service()
+service = Service(executable_path='/usr/lib/chromium-browser/chromedriver')
 options = webdriver.ChromeOptions()
 options.add_argument("--headless=new")
 driver = webdriver.Chrome(service=service, options=options)
 
+# Workaround for item not clickable
+# https://www.testim.io/blog/selenium-element-is-not-clickable-at-point/
+driver.maximize_window()
+
 # navigate to the target webpage
 driver.get("https://lms.handwerkswettbewerb.de/ibt/myso/cty/area=site/de/bin/public/voting/details.ibtsico?path=ibt:/division/myso/op/master/voting/2024/1246419951")
 
+#print(driver.page_source)
+
 # Navigate to second page and vote
-voteButton = driver.find_element("xpath", '//*[@title="Stimme abgeben"]')
-voteButton.click();
-voteButton2 = driver.find_element("xpath", '//*[@title="Stimme abgeben"]')
-voteButton2.click();
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@title='Stimme abgeben']"))).click()
+driver.implicitly_wait(2)
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@title='Stimme abgeben']"))).click()
+driver.implicitly_wait(2)
 
 # Try to get success message
 resultMessage = driver.find_element(By.CLASS_NAME, "u-value")
@@ -31,4 +38,3 @@ print(resultMessage.get_attribute('innerHTML'))
 
 # close the browser
 driver.quit()
-
